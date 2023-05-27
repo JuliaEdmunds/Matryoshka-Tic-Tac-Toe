bbe939f7be6024ffb7de0e10d8acc5402161a3d6
JuliaEdmunds <julia.edmunds112@gmail.com>
2023-05-27 14:24:52 +0800

    Testing setup

--- Assets/Scripts/Logic/GameSettings.cs
+++ Assets/Scripts/Logic/GameSettings.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 public static class GameSettings
 {
-    public static EPlayerType BluePlayer = EPlayerType.Invalid;
+    //TODO: Switch players to Invalid after testing
+    public static EPlayerType BluePlayer = EPlayerType.Human;
 
-    public static EPlayerType RedPlayer = EPlayerType.Invalid;
+    public static EPlayerType RedPlayer = EPlayerType.BasicAI;
 
     public static EPlayerColour Winner = EPlayerColour.Invalid;
 }
